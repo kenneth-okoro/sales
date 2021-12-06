3070e88dec4e8a03f27cfdca2e7cb00e066ca328
kenneth-okoro <kennethokesiena@gmail.com>
2021-12-06 20:03:18 +0100

    updated files

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-// local reviews data
+// testimonial data
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
+    name: "Kenneth Okoli",
     job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    img: "https://res.cloudinary.com/kenr0d/image/upload/v1638814439/demo/kenneth_zckyh2.jpg",
+    text: "I earn passively using the ropes Justice showed me while I go about my daily job. He's a down to earth and straight forward person that I'm glad I took a shot for.",
   },
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    name: "Elohor Jasmine",
+    job: "Hair Stylist",
+    img: "https://res.cloudinary.com/kenr0d/image/upload/v1638814553/demo/elohor_uqdvee.jpg",
+    text: "Justice teaching skills is next to none. In spite of my busy schedule, he made the learning curve an easy one. I earn from my primary work and side hustle and that's a win-win for me.",
   },
   {
     id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    name: "Isaiah Edet",
+    job: "Banker",
+    img: "https://res.cloudinary.com/kenr0d/image/upload/v1638814951/demo/kiel_g5wrmi.jpg",
+    text: "I've made quite a handsome amount of money from Justice mentorship. I got to learn that smart work is way better than hard work.",
   },
   {
     id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    name: "Daniel Osajini",
+    job: "Blogger",
+    img: "https://res.cloudinary.com/kenr0d/image/upload/v1638814295/demo/daniel-ohuabunwa_wzgo7q.jpg",
+    text: "Crossing paths with Justice is one adventure that'll last a lifetime because he opened my eyes to passive ways of earning I wasn't aware of.",
   },
 ];
+
 // select items
 const img = document.getElementById("person-img");
 const author = document.getElementById("author");
@@ -45,7 +38,6 @@ const info = document.getElementById("info");
 
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
-const randomBtn = document.querySelector(".random-btn");
 
 // set starting item
 let currentItem = 0;
@@ -67,6 +59,7 @@ function showPerson(person) {
   job.textContent = item.job;
   info.textContent = item.text;
 }
+
 // show next person
 nextBtn.addEventListener("click", function () {
   currentItem++;
@@ -75,18 +68,12 @@ nextBtn.addEventListener("click", function () {
   }
   showPerson(currentItem);
 });
+
 // show prev person
 prevBtn.addEventListener("click", function () {
   currentItem--;
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
   }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener("click", function () {
-  console.log("hello");
-
-  currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);
 });
